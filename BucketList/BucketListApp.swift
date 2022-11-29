d43bef8e8f33d7a5dac5d3dbf605adc416983e17
kiran sonne <kiransonne04@gmail.com>
2022-11-29 15:33:10 +0530

    Initial Commit

---
+++ BucketList/BucketListApp.swift
@@ -0,0 +1,17 @@
+//
+//  BucketListApp.swift
+//  BucketList
+//
+//  Created by Kiran Sonne on 29/11/22.
+//
+
+import SwiftUI
+
+@main
+struct BucketListApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
